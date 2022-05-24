83f748d3b5ce45d1ae726b406ef26af26396f7e4
Pete Bond <pgbondy@gmail.com>
2022-05-24 20:08:03 +0100

    Trains over multiple epochs. Logs to Tensorboard.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from skimage import io
 import torchvision.transforms as transforms
 import torch.nn.functional as F
@@ -34,38 +35,67 @@ dataset = ProductImageCategoryDataset()
 # dataset[10187]
 
 # %%
-train_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+train_loader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=0)
 # %%
 class CNN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.layers = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 8, 9),
-            torch.nn.Flatten(),  # flatten
-            # simplify further with linear layers
-            torch.nn.Linear(25088, 13),
-            torch.nn.Softmax()  # turn into probabilities
-        )
+        self.conv1 = torch.nn.Conv2d(3, 6, 5)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        self.fc1 = torch.nn.Linear(2704, 120)
+        self.fc2 = torch.nn.Linear(120, 84)
+        self.fc3 = torch.nn.Linear(84, 14)
+        torch.nn.Softmax(dim=1)        
 
-    def forward(self, features):
-        return self.layers(features)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)        
+        return x
 # %%
 
 model = CNN()
-optimizer = torch.optim.SDG(model.parameters(), lr=0.001)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+writer = SummaryWriter()
+writer2 = SummaryWriter()
+
+
 
 # %%
-for batch_idx, batch in enumerate(train_loader):
-    # print(batch)
-    features, labels = batch
-    prediction = model(features)
-    # calculate the loss
-    loss = F.cross_entropy(prediction, labels)
-    print("Loss: ", loss, "\t Batch: ", batch_idx)
-    
-    # backpropagate the loss
-    loss.backward()
-    # take an optimization step
-    break
+num_epochs = 100
+batch_losses = []
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for batch_idx, batch in enumerate(train_loader, 0):
+        # print(batch)
+        features, labels = batch
+        # zero the gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = model(features)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # write to tensorboard
+        writer.add_scalar('Loss', loss, batch_idx)
+        running_loss += loss.item()
+        batch_losses.append(loss.item())
+        if batch_idx % 200 == 199:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, batch_idx + 1, running_loss / 200))
+            running_loss = 0.0
+        #break
+    avg_loss = sum(batch_losses[-3:])/3
+    print('epoch: %d, avg_loss: %.3f' % (epoch + 1, avg_loss))
+    writer2.add_scalar('Avg Loss', avg_loss, epoch)
+writer.flush()
+writer2.flush()
+
 # %% 
-print(prediction)
+print(outputs)
